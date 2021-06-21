8ba6362c762ca553cd12e0756ac1efc06b10f096
이효진 <billion109@naver.com>
2021-06-21 23:07:00 +0900

    Initial commit

---
+++ src/main/java/com/example/studymates/StudymatesApplication.java
@@ -0,0 +1,13 @@
+package com.example.studymates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudymatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudymatesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studymates/StudymatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studymates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudymatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
